525a951aecc890099036c8593c003f8b007c9309
jomhlb <johannamuhlbergerr@gmail.com>
2025-09-17 11:57:30 +0200

    Optimisation images

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,19 +1,35 @@
-const multer = require('multer')
+const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 }
 
-const storage = multer.diskStorage({
-    destination: (req, file, callback) =>{
-        callback(null, 'images')
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
-    }
-})
+const storage = multer.memoryStorage();
 
-module.exports = multer ({ storage }).single('image');
+const upload = multer({ storage }).single('image');
+
+const processImage = (req, res, next) => {
+  if (!req.file) return next();
+
+  const extension = MIME_TYPES[req.file.mimetype];
+  const fileName = req.file.originalname.split(' ').join('_') + Date.now() + '.' + extension;
+  const filePath = path.join('images', fileName);
+
+  sharp(req.file.buffer)
+    .resize({ width: 800 })
+    .toFormat('jpeg')
+    .jpeg({ quality: 80 })
+    .toFile(filePath, (err) => {
+      if (err) return next(err);
+
+      req.file.filename = fileName;
+      next();
+    });
+};
+
+module.exports = { upload, processImage };
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth'); 
-const multer = require('../middleware/multer-config'); 
+const { upload, processImage } = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('/bestrating', bookCtrl.getBestRatingBooks);
 router.get('/:id', bookCtrl.getBookById);
 router.get('/', bookCtrl.getAllBooks);
 
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.updateBook);
+router.post('/', auth, upload, processImage, bookCtrl.createBook);
+router.put('/:id', auth, upload, processImage, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.addRating);
 
